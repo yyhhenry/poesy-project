1eb3f2f3aaa4bb99ee8eee91156256c9a94c490e
yyhhenry <yyhhenry@foxmail.com>
2024-07-12 21:04:07 +0800

    feat: validate pagination request in QuestionService

--- poesy-question/src/main/java/cn/d619/poesy/question/service/QuestionService.java
+++ poesy-question/src/main/java/cn/d619/poesy/question/service/QuestionService.java
@@ -1,8 +1,10 @@
 package cn.d619.poesy.question.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import cn.d619.poesy.question.exception.HttpException;
 import cn.d619.poesy.question.mapper.QuestionMapper;
 import cn.d619.poesy.question.pojo.dto.PaginationRequest;
 import cn.d619.poesy.question.pojo.dto.QuestionBriefDTO;
@@ -12,6 +14,15 @@ import cn.d619.poesy.question.pojo.po.QuestionPO;
 public class QuestionService {
     @Autowired
     private QuestionMapper questionMapper;
+
+    private void ensureValidPaginationRequest(PaginationRequest paginationRequest) {
+        if (paginationRequest.getLimit() <= 0 || paginationRequest.getStart() < 0) {
+            throw new HttpException(HttpStatus.BAD_REQUEST, "Invalid pagination request");
+        }
+        if (paginationRequest.getLimit() > 15) {
+            throw new IllegalArgumentException("Limit too large");
+        }
+    }
 
     public String addQuestion(String title, String content, String authorEmail) {
         QuestionPO questionPO = new QuestionPO(title, content, authorEmail);
@@ -24,11 +35,13 @@ public class QuestionService {
     }
 
     public QuestionBriefDTO[] questionsBy(PaginationRequest paginationRequest) {
+        ensureValidPaginationRequest(paginationRequest);
         /// TODO: implement this method
         throw new UnsupportedOperationException();
     }
 
     public QuestionBriefDTO[] latestQuestions(PaginationRequest paginationRequest) {
+        ensureValidPaginationRequest(paginationRequest);
         /// TODO: implement this method
         throw new UnsupportedOperationException();
     }
