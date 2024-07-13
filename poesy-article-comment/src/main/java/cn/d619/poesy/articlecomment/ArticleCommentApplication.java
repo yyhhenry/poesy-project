213f5e4f22ae1d181f5ca4846770b2de44c7493c
yyhhenry <yyhhenry@foxmail.com>
2024-07-13 22:06:59 +0800

    reset launch and fix entry

--- poesy-article-comment/src/main/java/cn/d619/poesy/articlecomment/ArticleCommentApplication.java
+++ poesy-article-comment/src/main/java/cn/d619/poesy/articlecomment/ArticleCommentApplication.java
@@ -1,12 +1,11 @@
 package cn.d619.poesy.articlecomment;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.wavefront.WavefrontProperties.Application;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ArticleCommentApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(ArticleCommentApplication.class, args);
     }
 }
