543e5478068880fb9f3ab8e7a51c1a43bee54206
颜哲 张 <305669115@qq.com>
2024-07-13 12:10:19 +0800

    feat(article-service): introduce pagination request validation
    Add new methods to validate PaginationRequest objects in ArticleService. This includes checks for
    valid limit and start values to prevent invalid pagination requests. Also, introduce a new
    IllegalArgumentException for cases when the limit is too large.

--- poesy-article/src/main/java/cn/d619/poesy/article/service/ArticleService.java
+++ poesy-article/src/main/java/cn/d619/poesy/article/service/ArticleService.java
@@ -1,15 +1,28 @@
 package cn.d619.poesy.article.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import cn.d619.poesy.article.exception.HttpException;
 import cn.d619.poesy.article.mapper.ArticleMapper;
+import cn.d619.poesy.article.pojo.dto.PaginationRequest;
+import cn.d619.poesy.article.pojo.dto.ArticleBriefDTO;
 import cn.d619.poesy.article.pojo.po.ArticlePO;
 
 @Service
 public class ArticleService {
     @Autowired
     private ArticleMapper articleMapper;
+
+    private void ensureValidPaginationRequest(PaginationRequest paginationRequest) {
+        if (paginationRequest.getLimit() <= 0 || paginationRequest.getStart() < 0) {
+            throw new HttpException(HttpStatus.BAD_REQUEST, "Invalid pagination request");
+        }
+        if (paginationRequest.getLimit() > 15) {
+            throw new IllegalArgumentException("Limit too large");
+        }
+    }
 
     public String addArticle(String title, String content, String authorEmail) {
         ArticlePO articlePO = new ArticlePO(title, content, authorEmail);
@@ -19,5 +32,17 @@ public class ArticleService {
 
     public ArticlePO getArticle(String id) {
         return articleMapper.selectById(id);
+    }
+
+    public ArticleBriefDTO[] articlesBy(PaginationRequest paginationRequest) {
+        ensureValidPaginationRequest(paginationRequest);
+        /// TODO: implement this method
+        throw new UnsupportedOperationException();
+    }
+
+    public ArticleBriefDTO[] latestArticles(PaginationRequest paginationRequest) {
+        ensureValidPaginationRequest(paginationRequest);
+        /// TODO: implement this method
+        throw new UnsupportedOperationException();
     }
 }
