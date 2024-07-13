db340314b4a2c25977891903129afebda8914b1a
202100800135 <3454485202@qq.com>
2024-07-13 21:34:49 +0800

    feat(poesy-article-comment): add AddArticleCommentDTO and update launch config
    - Create AddArticleCommentDTO to handle data transfer for new article comments.
    - Remove outdated launch configuration for 'ArticleCommentApplication' and
      update active configurations to align with current project structure.

---
+++ poesy-article-comment/src/main/java/cn/d619/poesy/articlecomment/pojo/dto/AddArticleCommentDTO.java
@@ -0,0 +1,27 @@
+package cn.d619.poesy.articlecomment.pojo.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
+public class AddArticleCommentDTO {
+    private String id;
+    private String content;
+    private String authorEmail;
+    private String articleId;
+    private LocalDateTime createdTime;
+
+    public AddArticleCommentDTO() {
+    }
+
+    public AddArticleCommentDTO(String id, String content, String authorEmail,
+            String articleId, LocalDateTime createdTime) {
+        this.id = id;
+        this.content = content;
+        this.authorEmail = authorEmail;
+        this.articleId = articleId;
+        this.createdTime = createdTime;
+    }
+
+}
