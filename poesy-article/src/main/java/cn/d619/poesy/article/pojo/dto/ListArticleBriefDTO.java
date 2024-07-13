399c5c13b456c471ca798690d4f0d4df25acd898
yyhhenry <yyhhenry@foxmail.com>
2024-07-13 16:14:41 +0800

    fix typo

--- poesy-article/src/main/java/cn/d619/poesy/article/pojo/dto/ListArticleBriefDTO.java
+++ poesy-article/src/main/java/cn/d619/poesy/article/pojo/dto/ListArticleBriefDTO.java
@@ -6,9 +6,9 @@ import java.util.List;
 
 @Data
 public class ListArticleBriefDTO {
-    private List<ArticleBriefDTO> questionBriefs;
+    private List<ArticleBriefDTO> articleBriefs;
 
-    public ListArticleBriefDTO(List<ArticleBriefDTO> questionBriefs) {
-        this.questionBriefs = questionBriefs;
+    public ListArticleBriefDTO(List<ArticleBriefDTO> articleBriefs) {
+        this.articleBriefs = articleBriefs;
     }
 }
