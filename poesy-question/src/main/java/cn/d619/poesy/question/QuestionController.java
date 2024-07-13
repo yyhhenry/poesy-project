9b21e0ef3f29c8a4942dbdab3995da4e23008c4c
yyhhenry <yyhhenry@foxmail.com>
2024-07-13 15:25:32 +0800

    refactor(question): remove unnecessary authentication check in questionsBy API

--- poesy-question/src/main/java/cn/d619/poesy/question/QuestionController.java
+++ poesy-question/src/main/java/cn/d619/poesy/question/QuestionController.java
@@ -51,17 +51,8 @@ public class QuestionController {
     }
 
     @GetMapping("/api/question/by-user")
-    public ListQuestionBriefDTO questionsBy(@RequestParam("email") String email,
-            @RequestHeader("Authorization") String auth) {
-        if (auth == null || !auth.startsWith("Bearer ")) {
-            throw new HttpException(HttpStatus.UNAUTHORIZED, "Missing or invalid token");
-        }
-        String token = auth.substring(7); // remove "Bearer "
-        jwtUtil.validateTokenWithType(token, "access");
-
+    public ListQuestionBriefDTO questionsBy(@RequestParam("email") String email) {
         return new ListQuestionBriefDTO(questionService.questionsBy(email));
-
-        // throw new UnsupportedOperationException();
     }
 
     @GetMapping("/api/question/{id}")
